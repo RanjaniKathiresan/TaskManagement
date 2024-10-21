f477c85a44dea4c0b3a81a2af102c775cbb34ceb
RanjaniKathiresan <ranjanikathiresan2012@gmail.com>
2024-10-21 08:59:37 +0530

    Added logout functionality

--- account/urls.py
+++ account/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('register/', views.register_user, name='register'),
     path('login/', views.custom_login, name='login'),
-    
+    path('logout/', views.CustomLogoutView.as_view(), name='logout'),    
 ]
--- account/views.py
+++ account/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm
 from django.contrib.auth import login, authenticate
 from django.contrib import messages
+from django.contrib.auth.views import LogoutView
+from django.urls import reverse_lazy
 
 def register_user(request):
     if request.method == 'POST':
@@ -27,3 +29,10 @@ def custom_login(request):
         else:
             messages.error(request, "Invalid username or password")
     return render(request, 'account/login.html')
+
+class CustomLogoutView(LogoutView):
+    next_page = reverse_lazy('login')
+    
+    def dispatch(self, request, *args, **kwargs):
+        response = super().dispatch(request, *args, **kwargs)
+        return response
